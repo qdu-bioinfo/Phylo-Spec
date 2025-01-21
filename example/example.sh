56600868b4170610034d674756405ffc0b2b7147
qdu-bioinfo <74221639+qdu-bioinfo@users.noreply.github.com>
2025-01-21 12:07:41 +0800

    Delete example/example.sh

--- example/example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python ./src/model/PhyloSpec_train_test.py -t ./example/Unclassified/phylogeny.nwk -c ./example/Unclassified/example_train.csv -taxo ./example/Unclassified/example_taxonomy.csv --PhyloSpec train
-
-python ./src/model/PhyloSpec_train_test.py -t ./example/Unclassified/phylogeny.nwk -c ./example/Unclassified/example_test.csv -taxo ./example/Unclassified/example_taxonomy.csv --PhyloSpec test
-
